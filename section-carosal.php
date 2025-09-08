cb6c70044ae5efb24d58dca0bf9ab58c60ea0658
Sardar Ali <sardaralikhamosh@gmail.com>
2025-09-08 23:36:05 +0500

    php
    php core file uploaded to the repository

---
+++ section-carosal.php
@@ -0,0 +1,87 @@
+<div class="bok-full">
+  <div class="owl-slider">
+    <div id="carousel" class="owl-carousel">
+      <div class="item">
+        <div class="bok-inner">
+          <!-- <h3><a href="https://escapethevault.co.uk" target="_blank">ESCAPE THE VAULT</a></h3> -->
+          <a href="https://escapethevault.co.uk" target="_blank">
+          <img src="https://images.squarespace-cdn.com/content/66f7f9f3950d253597abd8c4/50ec32f5-0131-4fa9-9a16-731aeb9865dc/Escape+the+Vault.jpg" /></a>
+         <!--  <button style="padding:3px; background:#244182;">
+          <a href="https://escapethevault.co.uk" target="_blank" style="background:#244182; color:#fff; margin:5px;">BOOK NOW</a>
+      </button> -->
+        </div>
+      </div>
+      <!-- tv game show -->
+       <!-- New slide with full width -->
+      <div class="item">
+        <div class="bok-inner">
+          <!-- <h3><a href="https://www.brightonmurdermystery.co.uk" target="_blank">THE PRINCE'S PANIC!</a></h3> -->
+          <a href="https://www.brightonmurdermystery.co.uk" target="_blank">
+          <img src="https://images.squarespace-cdn.com/content/66f7f9f3950d253597abd8c4/e98075c8-e557-4a2a-bad7-25dbd23857a6/Brighton+Murder+Mystery.jpg" /></a>
+          <!-- <button style="padding:3px; background:#244182;">
+          <a href="https://www.brightonmurdermystery.co.uk" target="_blank" style="background:#244182; color:#fff; margin:5px;">BOOK NOW</a>
+      </button> -->
+        </div>
+      </div>
+      <!-- End of new slide -->
+       <div class="item">
+        <div class="bok-inner">
+          <!-- <h3><a href="https://www.escapethecell.co.uk" target="_blank">ESCAPE THE CELL</a></h3> -->
+          <a href="https://www.escapethecell.co.uk" target="_blank">
+          <img src="https://images.squarespace-cdn.com/content/66f7f9f3950d253597abd8c4/dcd303b9-c5bd-4ca2-97d4-3940399d17b4/Escape+the+Cell.jpg" />
+        </a>
+          <!-- <button style="padding:3px; background:#244182;">
+          <a href="https://www.escapethecell.co.uk" target="_blank" style="background:#244182; color:#fff; margin:5px;">BOOK NOW</a>
+          </button> -->
+        </div>
+      </div>
+
+      <div class="item">
+        <div class="bok-inner">
+          <!-- <h3><a href="https://escapethevault.co.uk" target="_blank">ESCAPE THE VAULT</a></h3> -->
+          <a href="https://www.thetvgameshow.co.uk" target="_blank">
+          <img src="https://images.squarespace-cdn.com/content/66f7f9f3950d253597abd8c4/b442186d-56a3-405f-91f5-b0c9aa93ac28/TV+Game+Show.jpg" /></a>
+         <!--  <button style="padding:3px; background:#244182;">
+          <a href="https://escapethevault.co.uk" target="_blank" style="background:#244182; color:#fff; margin:5px;">BOOK NOW</a>
+      </button> -->
+        </div>
+      </div>
+      <!-- tv game show close -->
+    </div>
+  </div>
+</div>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+<script>
+  jQuery(document).ready(function() {
+    jQuery('#carousel').owlCarousel({
+      autoplay: true,
+      lazyLoad: true,
+      loop: true,
+      margin: 0, // Set margin to 0 for full width
+      responsiveClass: true,
+      autoHeight: true,
+      autoplayTimeout: 3000,
+      smartSpeed: 800,
+      nav: true,
+      dots: true,
+      responsive: {
+        0: {
+          items: 1
+        },
+        768: {
+          items: 1
+        },
+        1023: {
+          items: 1
+        },
+        1024: {
+          items: 1
+        }
+      }
+    });
+  });
+</script>
